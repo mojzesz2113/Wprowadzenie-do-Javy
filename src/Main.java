8c7f32d5dea4693cde1f53fd97c90a2d76ba72e8
mojzesz2113 <miloszgit2115@gmail.com>
2022-09-15 14:08:39 +0200

    statki

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,15 @@ public class Main {
         //w zbiorze elementy nie sa indeksowane
         Set<Integer> wylosowane = new HashSet<>();
         while(wylosowane.size()<6){
-            wylosowane.add((int)(Math.random()*10));
+            wylosowane.add((int)(Math.random()*100+1));
         }
-        System.out.println("wylosowane:"+wylosowane);
+        //System.out.println("wylosowane:"+wylosowane);
 
         //wczytywanie z klawiatury 6 liczb
         Scanner klawiatura = new Scanner(System.in);
         List<Integer> wpisane = new ArrayList<>();
+        //lista elementy moga sie powtarzac
+        //lista ma indeksowane elementy
         System.out.println("Podaj 6 liczb");
         for(int i=0;i<6;i++){
             System.out.println("Liczba "+i);
@@ -29,5 +31,14 @@ public class Main {
         }
         System.out.println("Wpisane "+wpisane);
 
+        //zapis listy ktora bedzie zawierala elementy
+        //ktore powtarzaja sie w wpisane i wylosowane
+        List<Integer> trafione = new LinkedList<>();
+        for(Integer wpisanaWartosc : wpisane){
+            if(wylosowane.contains(wpisanaWartosc)){
+                trafione.add(wpisanaWartosc);
+            }
+        }
+        System.out.println("Trafione"+trafione);
     }
 }
