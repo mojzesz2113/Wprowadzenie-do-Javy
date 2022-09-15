b99965b97bcb121ec8918e8f73fc6751f4173d05
mojzesz2113 <miloszgit2115@gmail.com>
2022-09-15 13:25:49 +0200

    wypelnienie listy - elementy moga sie powtarzac

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,5 +13,16 @@ public class Main {
             wylosowane.add((int)(Math.random()*10));
         }
         System.out.println("wylosowane:"+wylosowane);
+
+        //wczytywanie z klawiatury 6 liczb
+        Scanner klawiatura = new Scanner(System.in);
+        List<Integer> wpisane = new ArrayList<>();
+        System.out.println("Podaj 6 liczb");
+        for(int i=0;i<6;i++){
+            System.out.println("Liczba "+i);
+            wpisane.add(klawiatura.nextInt());
+        }
+        System.out.println("Wpisane "+wpisane);
+
     }
 }
